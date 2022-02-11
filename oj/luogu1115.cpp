f55855e514588ac47def9b0c16fc852683a05512
李富荣 <leefurong@gmail.com>
2022-02-11 20:43:44 +0800

    first try of 1115

---
+++ oj/luogu1115.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int dp[100001];
+int n;
+
+int main(){
+    cin >> n;
+    int maxx;
+    for(int i=1; i<=n; i++){
+        int a;
+        cin >> a;
+        dp[i] = max(dp[i-1], 0)+a;
+        if (i==1) maxx=dp[i];
+        maxx = max(dp[i], maxx);
+    }
+    cout << maxx;
+}
