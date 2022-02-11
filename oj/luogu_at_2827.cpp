90252a3360c120e0588130e7241b5d32dd82104c
李富荣 <leefurong@gmail.com>
2022-02-11 21:27:38 +0800

    luogu at 2827, 一个天真的尝试。

---
+++ oj/luogu_at_2827.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int n;
+int dp[100001];
+int a[100001];
+int main(){
+    cin >> n;
+    for(int i=1; i<=n; i++){
+        cin >> a[i];
+    }
+
+    for(int i=1; i<=n; i++){
+        int mx=0;
+        for(int j=1; j<i; j++){
+            if (a[j]<a[i]){
+                mx = max(mx, dp[j]);
+            }
+        }
+        dp[i] = mx+1;
+    }
+
+    cout << dp[n];
+}
--- oj/readme.md
+++ oj/readme.md
@@ -1,2 +1,3 @@
 ### dp
 * [1115 最大子段和](luogu1115.cpp)
+* [洛谷AT2827 LIS](luogu_at_2827.cpp)
