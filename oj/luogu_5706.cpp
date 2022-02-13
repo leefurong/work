6d96286b943283864762d2eb5cc2b415e0007401
李富荣 <leefurong@gmail.com>
2022-02-13 19:41:15 +0800

    妈呀，luogu 5706 终于过了。 也不知道数据是搞的什么鬼， prev、next不用， 反而能过。

--- oj/luogu_5706.cpp
+++ oj/luogu_5706.cpp
@@ -4,11 +4,13 @@ using namespace std;
 struct node{
     int k;
     int size;
-    node* left=NULL;
-    node* right=NULL;
+    int count;
+    node* left;
+    node* right;
     node(int _k){
         k=_k;
         size=1;
+        count=1;
     }
 };
 
@@ -21,29 +23,34 @@ node* insert(node* tree, int x){
     if (tree==NULL) return new node(x);
     if (x<tree->k){
         tree->left = insert(tree->left, x);
-    }else{
+    }else if (x>tree->k){
         tree->right = insert(tree->right, x);
+    }else{
+        tree->count++;
     }
-    tree->size=size(tree->left)+size(tree->right);
+    tree->size=size(tree->left)+size(tree->right)+tree->count;
     return tree;
 }
 
 int _rank(node * tree, int x){
+    // 不加这一句， 就会RE。 看来不知道什么数据，会找到树以外去了。
+    if (tree==NULL) return 1;
+
     if (tree->k == x){
         return size(tree->left)+1;
     }
     if (tree->k<x){
-        return size(tree->left)+1 + _rank(tree->right, x);
+        return size(tree->left)+tree->count + _rank(tree->right, x);
     }
     return _rank(tree->left, x);
 }
 
 int nth(node * tree, int n){
-    if (size(tree->left)+1==n){
+    if (n>=size(tree->left)+1 && n<=size(tree->left)+tree->count){
         return tree->k;
     }
-    else if (size(tree->left)+1<n){
-        return nth(tree->right, n-size(tree->left)-1);
+    else if (size(tree->left)+tree->count<n){
+        return nth(tree->right, n-size(tree->left)-tree->count);
     }
     else {
         return nth(tree->left, n);
@@ -76,14 +83,16 @@ node * tree=NULL;
 int main(){
     int n;
     cin >> n;
+    tree=insert(tree, 2147483647);
+    tree=insert(tree, -2147483647);
     while(n--){
         int cmd, x;
         cin >> cmd>>x;
         switch (cmd){
-            case 1: cout << _rank(tree, x)<<endl; break;
-            // case 2: cout << nth(tree, x) << endl; break;
-            case 3: cout << prev(tree, x)<<endl;break;
-            case 4: cout << next(tree, x)<<endl; break;
+            case 1: cout << _rank(tree, x)-1<<endl; break;
+            case 2: cout << nth(tree, x+1) << endl; break;
+            case 3: cout << nth(tree, _rank(tree, x)-1)<<endl;break;
+            case 4: cout << nth(tree, _rank(tree, x+1))<<endl; break;
             case 5: tree=insert(tree, x);break;
         }
     }
