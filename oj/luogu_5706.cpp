2f99a92e798372c645a654b33c3d9e96108cf297
李富荣 <leefurong@gmail.com>
2022-02-13 13:07:43 +0800

    luogu 5706 二叉搜索树（这个版本RE了）

---
+++ oj/luogu_5706.cpp
@@ -0,0 +1,93 @@
+#include<iostream>
+using namespace std;
+
+struct node{
+    int k;
+    int size;
+    node* left=NULL;
+    node* right=NULL;
+    node(int _k){
+        k=_k;
+        size=1;
+    }
+};
+
+int size(node* t){
+    if (t==NULL) return 0;
+    return t->size;
+}
+
+node* insert(node* tree, int x){
+    if (tree==NULL) return new node(x);
+    if (x<tree->k){
+        tree->left = insert(tree->left, x);
+    }else{
+        tree->right = insert(tree->right, x);
+    }
+    tree->size=size(tree->left)+size(tree->right);
+    return tree;
+}
+
+int _rank(node * tree, int x){
+    if (tree->k == x){
+        return size(tree->left)+1;
+    }
+    if (tree->k<x){
+        return size(tree->left)+1 + _rank(tree->right, x);
+    }
+    if (tree->k>x){
+        return _rank(tree->left, x);
+    }
+    return -1; // should not go here. just to supress warning.
+}
+
+int nth(node * tree, int n){
+    if (size(tree->left)+1==n){
+        return tree->k;
+    }
+    else if (size(tree->left)+1<n){
+        return nth(tree->right, n-size(tree->left)-1);
+    }
+    else {
+        return nth(tree->left, n);
+    }
+}
+
+int prev(node * tree, int x){
+    if (tree==NULL) return -2147483647;
+    if (tree->k < x && (!tree->right || tree->right->k>=x)){
+        return tree->k;
+    }
+    if (tree->k >= x){
+        return prev(tree->left, x);
+    }
+    return prev(tree->right, x);
+}
+
+
+int next(node * tree, int x){
+    if (tree==NULL) return 2147483647;
+    if (tree->k > x && (!tree->left || tree->left->k<=x)){
+        return tree->k;
+    }
+    if (tree->k <= x){
+        return next(tree->right, x);
+    }
+    return next(tree->left,x);
+}
+node * tree=NULL;
+int main(){
+    int n;
+    cin >> n;
+    while(n--){
+        int cmd, x;
+        cin >> cmd>>x;
+        switch (cmd){
+            case 1: cout << _rank(tree, x)<<endl; break;
+            case 2: cout << nth(tree, x) << endl; break;
+            case 3: cout << prev(tree, x)<<endl;break;
+            case 4: cout << next(tree, x)<<endl; break;
+            case 5: tree=insert(tree, x);break;
+        }
+    }
+}
--- oj/readme.md
+++ oj/readme.md
@@ -1,6 +1,14 @@
 ### dp
 * [1115 最大子段和](luogu1115.cpp)
 * [洛谷AT2827 LIS](luogu_at_2827.cpp)
+
+# 循环、字符串、枚举
 * [中英文识别] (cn_en.cpp)
+
+# 递推、递归
 * [洛谷2437 蜜蜂路线](luogu_2437.cpp)
-* [阶乘intro](jiecheng_intro.cpp)
+* [阶乘intro 递归实现阶乘](jiecheng_intro.cpp)
+* [阶乘ditui 递归实现阶乘](jiecheng_ditui.cpp)
+
+# 二叉树
+[洛谷5706 普通二叉树（简化版）](luogu_5706.cpp)
