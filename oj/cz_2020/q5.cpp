dbc9d64dc2f2b1e7f3f434b9f3cfaa905f26e7f7
李富荣 <leefurong@gmail.com>
2022-02-16 17:46:34 +0800

    常州2020市赛第五题： 采用高精。现在能完全AC了。

--- oj/cz_2020/q5.cpp
+++ oj/cz_2020/q5.cpp
@@ -1,18 +1,58 @@
 #include<iostream>
 #include<algorithm>
+#include<cstring>
 using namespace std;
 long long atk, hp;
 int n;
 long long ati[100001];
+const int sz=30;
+int total_atk[sz];
+int temp[sz];
+
+void to_big_temp(long long x){
+    memset(temp, 0, sizeof(temp));
+    string sx = to_string(x);
+    for(int i=0; i<sx.size(); i++){
+        temp[i] = sx[i]-'0';
+    }
+    reverse(temp, temp+sx.size());
+}
+
+void big_add(long long a){
+    to_big_temp(a);
+    for(int i=0; i<sz; i++){
+        total_atk[i] += temp[i];
+    }
+    for(int i=0; i<sz-1; i++){
+        total_atk[i+1] += total_atk[i]/10;
+        total_atk[i]%=10;
+    }
+}
+/**
+ *  Is total_atk greater or equal than x?
+ **/
+bool big_ge(long long x){
+    to_big_temp(x);
+    int res=0;
+    for(int i=20; i>=0; i--){
+        if (temp[i]!=total_atk[i]){
+            return total_atk[i]>temp[i]; // greater than
+        }
+    }
+    return true; // equals.
+}
 
 bool canwin(int beg){
-    long long total_atk=0;
+    memset(total_atk, 0, sizeof(total_atk));
     int k=1;
     for(int i=beg;i<=n;i++){
-        total_atk += (ati[i]*k);
+        big_add(ati[i]*k);
         k++;
     }
-    return total_atk>=hp;
+    // 既然total_atk有可能大于hp， 而hp是一个long long， 也就是说，
+    // 万一数据中hp竟然是最大的long long， 那么totak_atk用long long就可能溢出。
+    // 所以用高精来表达total_atk
+    return big_ge(hp);
 }
 
 // 检查中间位置center， 这个位置及以后的勇士都被招募。
