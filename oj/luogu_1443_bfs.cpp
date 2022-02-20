7117838a00ee7eeb34cecd69c4b5d0807fda5954
李富荣 <leefurong@gmail.com>
2022-02-20 09:59:52 +0800

    马的遍历（洛谷1443）bfs的优化： 不需要用vis， 因为steps已经包含了这个地方有没有走过的信息。（如果走过， 就不是-1）

--- oj/luogu_1443_bfs.cpp
+++ oj/luogu_1443_bfs.cpp
@@ -14,7 +14,6 @@ struct coord{
 int m, n, x, y;
 
 int steps[401][401];
-int vis[401][401];
 
 void print(){
     for(int i=1; i<=n; i++){
@@ -28,7 +27,6 @@ void bfs(int x1, int y1){
     memset(steps, -1, sizeof(steps));
     queue<coord> q;
     q.push(coord(x1, y1));
-    vis[x1][y1]=1;
     steps[x1][y1] = 0;
 
     while(!q.empty()){
@@ -39,12 +37,10 @@ void bfs(int x1, int y1){
                 if(dx!=0 && dy!=0 && abs(dx)!=abs(dy)){
                     int next_x = pos.x+dx;
                     int next_y = pos.y+dy;
-                    if (!vis[next_x][next_y] && next_x>=1 && next_y>=1 && next_x<=n && next_y<=m){
+                    if (steps[next_x][next_y]==-1 && next_x>=1 && next_y>=1 && next_x<=n && next_y<=m){
                         q.push(coord(next_x, next_y));
-                        vis[next_x][next_y] = 1;
                         steps[next_x][next_y] = steps[pos.x][pos.y]+1;
                     }
-
                 }
             }
         }
