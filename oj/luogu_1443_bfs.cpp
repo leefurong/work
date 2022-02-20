9665a271dc9bb95e325c42f8c11e8e2086f07696
李富荣 <leefurong@gmail.com>
2022-02-20 09:55:21 +0800

    马的遍历（洛谷1443）： 改进的bfs： 只需要跑一遍， 就能搞定所有的格子。 解决了TLE问题。 然而又有一个地方出现WA了。

--- oj/luogu_1443_bfs.cpp
+++ oj/luogu_1443_bfs.cpp
@@ -16,12 +16,18 @@ int m, n, x, y;
 int steps[401][401];
 int vis[401][401];
 
-int bfs(int x1, int y1, int x2, int y2){
-    if (x2==x1 && y2==y1) return 0;
-    memset(steps, 0, sizeof(steps));
-    memset(vis, 0, sizeof(vis));
+void print(){
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=m; j++){
+            cout << left<<setw(5)<<steps[i][j];
+        }
+        cout <<endl;
+    }
+}
+void bfs(int x1, int y1){
     queue<coord> q;
     q.push(coord(x1, y1));
+    vis[x1][y1]=1;
 
     while(!q.empty()){
         coord pos = q.front();q.pop();
@@ -32,28 +38,19 @@ int bfs(int x1, int y1, int x2, int y2){
                     int next_x = pos.x+dx;
                     int next_y = pos.y+dy;
                     if (!vis[next_x][next_y] && next_x>=1 && next_y>=1 && next_x<=n && next_y<=m){
-                        if (next_x==x2 && next_y==y2){
-                            return steps[pos.x][pos.y]+1;
-                        }else{
-                            q.push(coord(next_x, next_y));
-                            vis[next_x][next_y] = 1;
-                            steps[next_x][next_y] = steps[pos.x][pos.y]+1;
-                        }
+                        q.push(coord(next_x, next_y));
+                        vis[next_x][next_y] = 1;
+                        steps[next_x][next_y] = steps[pos.x][pos.y]+1;
                     }
 
                 }
             }
         }
     }
-    return -1;
 }
 
 int main(){
     cin >> n>> m>>x>>y;
-    for(int i=1; i<=n; i++){
-        for(int j=1;j<=m; j++){
-            cout << left<<setw(5)<<bfs(x, y, i, j);
-        }
-        cout << endl;
-    }
+    bfs(x, y);
+    print();
 }
