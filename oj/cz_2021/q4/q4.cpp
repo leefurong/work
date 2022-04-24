8f6e1cf58b701680089dd70ff67a7fc258438aad
李富荣 <leefurong@gmail.com>
2022-04-24 23:37:21 +0800

    cz2021 q4 代码更新： 用虚拟位置的思想来做。从而支持多次出现特殊字符； 也比暴力替换字符串快。
    目前还没能完全通过。

--- oj/cz_2021/q4/q4.cpp
+++ oj/cz_2021/q4/q4.cpp
@@ -3,45 +3,34 @@ using namespace std;
 int n, k;
 string s;
 
-// 当特殊字符为c时， s破译后的第k位是什么？
 char solve(char c){
-    int cbeg, cend;
-    int i=0;
-    while(s[i]!=c && i<n){
-        i++;
-        if (i == k + 1){ // k在特殊符号之前
-            return s[k];
+    int vi=-1; // 虚拟位置
+    int i=0; // 待检查的位置
+    while(i<n){
+        while(i<n&&s[i]!=c){
+            vi++;
+            if(vi==k){
+                return s[i];
+            }
+            i++;
         }
-    }
-    cbeg = i;
-    while(s[i]==c && i<n){
-        i++;
-    } 
-    cend = i;
-    
-    int clen=cend-cbeg;
-    printf("%d...%d: %d", cbeg, cend, clen);
-    if (clen==0){
-        return k<n?s[k]: '*'; // 木有特殊符号
-    } else {
-        string replacement = s.substr(cend, clen);
-        int sz = replacement.size();
-        // 如果k在replacement重复clen次之中...
-        // 这种情况下， sz不可能为0. 否则k会在replacement重复之后。
-        if (k<cbeg+clen*sz){ 
-            int targ_pos = (k-cbeg)%sz;
-            return replacement[targ_pos];
-        }else
-        { // 否则，k在replacement重复clen之后...
-        // k前面的字符串， 破解后增加了多少？
-            int diff = -clen-sz+sz*clen;
-            // 查找破解后的k位，想象一下， 从破解后还原成老字符串， 就把diff减去
-            int pos = k-diff;
-            return pos<n?s[pos]: '*';
+        int x=0;
+        while(i<n && s[i]==c){
+            x++;
+            i++;
         }
+        int len=min(x, n-i);
+        // 过了复制区域后， 位置是什么？
+        int nexti=i+len;
+        int nextvi=vi+len*x;
+        if (len>0) vi++;
+        if (nextvi>=k){
+            return s[i + (k - vi) % len];
+        }
+        i=nexti;
+        vi=nextvi;
     }
-
-
+    return '*';
 }
 
 int main(){
