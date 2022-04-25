517c6e43fcc90f741d0fa9cb2e611391d59a589f
李富荣 <leefurong@gmail.com>
2022-04-25 09:25:51 +0800

    把2021 q4整理的更简洁一些。（依然没有通过新驿站的oj)

--- oj/cz_2021/q4/q4.cpp
+++ oj/cz_2021/q4/q4.cpp
@@ -1,34 +1,47 @@
 #include<iostream>
+#define go(a, b) char ans=move(a, b); if(ans) return ans;
 using namespace std;
 int n, k;
 string s;
+int i;
+int vi;
+// move i by di, with vi by repeatVi*di
+// During this movement, if k==vi, return s[i] and stop movement.
+// Otherwise, returns '\0'
+// If reach end of string, returns '*'
+char move(int di, int repeatVi){
+    if (i>=n){
+        return '*';
+    }
+    if (repeatVi>0 && vi==k){
+        return s[i];
+    }
+    int vi2 = vi + di*repeatVi;
+    int i2 = i+di;
+    if (k<vi2){
+        int distance = k-vi;
+        return s[i+distance%di];
+    }
 
+    vi=vi2;
+    i=i2;
+    return 0;
+}
 char solve(char c){
-    int vi=-1; // 虚拟位置
-    int i=0; // 待检查的位置
+    vi=0; // 虚拟位置
+    i=0; // 待检查的位置
     while(i<n){
         while(i<n&&s[i]!=c){
-            vi++;
-            if(vi==k){
-                return s[i];
-            }
-            i++;
+            go(1,1);
         }
         int x=0;
         while(i<n && s[i]==c){
             x++;
-            i++;
+            go(1,0);
         }
+      
         int len=min(x, n-i);
-        // 过了复制区域后， 位置是什么？
-        int nexti=i+len;
-        int nextvi=vi+len*x;
-        if (len>0) vi++;
-        if (nextvi>=k){
-            return s[i + (k - vi) % len];
-        }
-        i=nexti;
-        vi=nextvi;
+        go(len, x);
     }
     return '*';
 }
