ffe7eda72b00a1ee0e9ff0e4134938f0f4895649
李富荣 <leefurong@gmail.com>
2022-04-26 11:25:06 +0800

    2021 cz q4: 特殊字符： 通过了曹文劳模工作室的oj

--- oj/cz_2021/q4/q4.cpp
+++ oj/cz_2021/q4/q4.cpp
@@ -1,46 +1,26 @@
 #include<iostream>
-#define go(a, b) char ans=move(a, b); if(ans) return ans;
 using namespace std;
 int n, k;
 string s;
 int i;
 int vi;
-// move i by di, with vi by repeatVi*di
-// During this movement, if k==vi, return s[i] and stop movement.
-// Otherwise, returns '\0'
-// If reach end of string, returns '*'
-char move(int di, int repeatVi){
-    // 移动之前，先看是否已经有答案了。
-    if (i>=n){ // 答案1： 越界了。
-        return '*';
-    } else {
-        int vi2 = vi + di*repeatVi;
-        int i2 = i+di;
-        if (vi!=vi2 && k<vi2){ // 答案2： 答案在此次移动的终点内, 且本次有效移动v有变化。(如果没变化， 不算计数)
-            int distance = k-vi;
-            return s[i+distance%di];
-        }
-        // 移动
-        vi=vi2;
-        i=i2;
-        return 0;
-    }
-}
+
 char solve(char c){
-    vi=0; // 虚拟位置
-    i=0; // 待检查的位置
-    while(i<n){
-        while(i<n&&s[i]!=c){
-            go(1,1);
+    int vi=0;
+    for(int i=0; i<n; i++){
+        if (s[i]!=c){
+            if (vi==k){
+                return s[i];
+            }
+            vi++;
+        } else {
+            int x=0;
+            while(i<n && s[i]==c) ++x, ++i;
+            string ss=s.substr(i, x);
+            if(ss.size()*x+vi>k) return ss[(k-vi)%ss.size()];
+            vi+=ss.size()*x;
+            i+=x-1;
         }
-        int x=0;
-        while(i<n && s[i]==c){
-            x++;
-            go(1,0);
-        }
-      
-        int len=min(x, n-i);
-        go(len, x);
     }
     return '*';
 }
